4c3656beadcfbbbaac3c7b42a6d46d5e482312a8
k-donn <mitchell@kevinandjudy.com>
2019-12-30 18:46:05 -0500

    Finialized refactoring and added license

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /*
 TODO
-- Refactor to class
+- Type hints?
 */
 
 class Logo {
@@ -44,12 +44,14 @@ class Logo {
 		console.log("Height Diff " + this.heightDiff);
 		console.log(
 			"gcd of WdthDiff and HgtDiff " +
-				gcd(this.widthDiff, this.heightDiff)
+				this.gcd(this.widthDiff, this.heightDiff)
 		);
 		console.log(
 			"lcm of WdthDiff and HgtDiff " +
-				lcm(this.widthDiff, this.heightDiff)
+				this.lcm(this.widthDiff, this.heightDiff)
 		);
+
+		this.findCorners();
 	}
 
 	/**
@@ -57,23 +59,26 @@ class Logo {
 	 */
 	findCorners() {
 		if (
-			Math.abs(this.x - this.y) % gcd(this.widthDiff, this.heightDiff) ===
+			Math.abs(this.x - this.y) %
+				this.gcd(this.widthDiff, this.heightDiff) ===
 			0
 		) {
 			// corners will be reached
 			if (
 				(Math.abs(this.x - this.y) /
-					gcd(this.widthDiff, this.heightDiff)) %
+					this.gcd(this.widthDiff, this.heightDiff)) %
 					2 ===
 				0
 			) {
 				this.corner1 =
-					((lcm(this.widthDiff, this.heightDiff) / this.heightDiff) %
+					((this.lcm(this.widthDiff, this.heightDiff) /
+						this.heightDiff) %
 						2 ===
 					0
 						? "T"
 						: "B") +
-					((lcm(this.widthDiff, this.heightDiff) / this.widthDiff) %
+					((this.lcm(this.widthDiff, this.heightDiff) /
+						this.widthDiff) %
 						2 ===
 					0
 						? "L"
@@ -89,12 +94,14 @@ class Logo {
 				console.groupEnd("corner-2");
 			} else {
 				this.corner1 =
-					((lcm(this.widthDiff, this.heightDiff) / this.heightDiff) %
+					((this.lcm(this.widthDiff, this.heightDiff) /
+						this.heightDiff) %
 						2 !==
 					0
 						? "T"
 						: "B") +
-					((lcm(this.widthDiff, this.heightDiff) / this.widthDiff) %
+					((this.lcm(this.widthDiff, this.heightDiff) /
+						this.widthDiff) %
 						2 !==
 					0
 						? "L"
@@ -126,7 +133,7 @@ class Logo {
 			return a;
 		}
 
-		return gcd(b, a % b);
+		return this.gcd(b, a % b);
 	}
 
 	/**
@@ -137,14 +144,63 @@ class Logo {
 	 * @returns {number} The lowest common multiple
 	 */
 	lcm(a, b) {
-		return Math.abs(a * b) / gcd(a, b);
+		return Math.abs(a * b) / this.gcd(a, b);
+	}
+
+	/**
+	 * Update canvas elements with the specified data.
+	 */
+	draw() {
+		this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+		this.context.fillStyle = "#000000";
+		this.context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+		this.context.fillStyle = "#00ff00";
+		this.context.fillRect(this.x, this.y, this.logoWidth, this.logoHeight);
+
+		if (this.t) {
+			this.period =
+				(this.t * this.lcm(this.widthDiff, this.heightDiff)) /
+				this.widthDiff;
+			this.context.fillStyle = "#ff0000";
+			this.context.font = "18px Courier New";
+			let timeToImpact =
+				(
+					(this.period -
+						((new Date().getTime() - this.start) % this.period)) /
+					1000
+				).toFixed(3) + " s.";
+			const periodTxt = (this.period / 1000).toFixed(3);
+			this.context.fillText(
+				`Period: ${this.corner1 ? periodTxt + "s." : "None"}`,
+				10,
+				20
+			);
+			this.context.fillText(
+				`Corner 1: ${this.corner1 || "None"}`,
+				235,
+				20
+			);
+
+			this.context.fillText(
+				`Corner 2: ${this.corner2 || "None"}`,
+				410,
+				20
+			);
+			this.context.fillText(
+				`Impact T-${this.corner1 ? timeToImpact : "None"}`,
+				600,
+				20
+			);
+
+			// debugger;
+		}
 	}
 
 	/**
 	 * Update the position of the logo and detect collision of the logo with the walls.
 	 */
 	animate() {
-		for (let i = 0; i < rate; i++) {
+		for (let i = 0; i < this.rate; i++) {
 			this.x += this.xVelocity;
 			this.y += this.yVelocity;
 
@@ -172,7 +228,7 @@ class Logo {
 			// Right
 			if (this.x + this.logoWidth === this.canvasWidth) {
 				if (!this.t) {
-					this.t = new Date().getTime() - start;
+					this.t = new Date().getTime() - this.start;
 				}
 				this.xVelocity = -this.xVelocity;
 			}
@@ -194,35 +250,13 @@ class Logo {
 	}
 
 	/**
-	 * Initialize the canvas and canvas elements.
+	 * Begin the animation
 	 */
-	draw() {
-		this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-		this.context.fillStyle = "#000000";
-		this.context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
-		this.context.fillStyle = "#00ff00";
-		this.context.fillRect(this.x, this.y, this.logoWidth, this.logoHeight);
-
-		if (this.t) {
-			this.period =
-				(this.t * lcm(this.widthDiff, this.heightDiff)) /
-				this.widthDiff;
-			this.context.fillStyle = "#ff0000";
-			this.context.font = "18px Courier New";
-			let timeToImpact =
-				(this.period - ((new Date().getTime() - start) % this.period)) /
-					1000 +
-				" s.";
-			this.context.fillText(
-				`Period: ${this.corner1 ? this.period / 1000 + "s." : "None"}
-					Corner 1: ${this.corner1 || "None"}
-					Corner 2: ${this.corner2 || "None"}
-					Impact T-${this.corner1 ? timeToImpact : "None"}`,
-				10,
-				20
-			);
-			// debugger;
-		}
+	startAnim() {
+		this.start = new Date().getTime();
+		setInterval(() => {
+			this.animate();
+		}, 1000 / 60);
 	}
 }
 
@@ -230,8 +264,7 @@ let el = document.getElementById("mockScreen");
 let ctx = el.getContext("2d");
 ctx.canvas.width = 800;
 ctx.canvas.height = 600;
+
 let logo = new Logo(el, ctx, ctx.canvas.width, ctx.canvas.height, 140, 140, 5);
 
-// animate();
-start = new Date().getTime();
-setInterval(animate, 1000 / 60);
+logo.startAnim();
